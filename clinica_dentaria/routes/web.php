552c84141ec96a6945aa7ca90d38c53841160fa2
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-04-24 11:14:55 -0700

    Routes & Layout
    Atualizacao de rotas e uso de yield e section e extend nas paginas do projecto

--- clinica_dentaria/routes/web.php
+++ clinica_dentaria/routes/web.php
@@ -12,7 +12,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//rota para a pagina inicial
 Route::get('/', function () {
     return view('index');
 });
+//rota para a pagina de doutores
+Route::get('/doutores', function () {
+    return view('doutores');
+});
+//rota para a pagina galeria
+Route::get('/galeria', function () {
+    return view('galeria');
+});
+//rota para a pagina sobre a empresa
+Route::get('/sobre', function () {
+    return view('sobre');
+});
+//rota para a pagina servicos
+Route::get('/servico', function () {
+    return view('servico');
+});
+//rota para a pagina de contacto
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
