2006f434b8e4d2f28a2472af484d3a44f19aeff8
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2023-05-13 01:26:04 -0700

    Update clinica_dentaria.sql

--- clinica_dentaria.sql
+++ clinica_dentaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2023 at 12:05 PM
+-- Generation Time: May 13, 2023 at 10:24 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,234 +24,282 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `agendamento`
+-- Table structure for table `appointments`
 --
 
-CREATE TABLE `agendamento` (
-  `id` int(11) NOT NULL,
-  `id_paciente` int(11) NOT NULL,
-  `data` date NOT NULL,
-  `hora` varchar(220) NOT NULL,
-  `id_especialidade` int(11) NOT NULL,
-  `id_medico` int(11) NOT NULL,
-  `id_status` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `appointments` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `doctor` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `message` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `name`, `email`, `phone`, `doctor`, `date`, `message`, `status`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 'Cripton Domingos Baloi', 'criptonbaloi19@gmail.com', '846594587', 'Gustavo', '2023-05-16', 'Meu dente esta inchado', 'Aprovado', '1', '2023-05-13 09:46:02', '2023-05-13 14:00:45'),
+(3, 'Andre Chissano', 'andre@gmail.com', '876590548', 'Francisco', '2023-05-18', 'Carie dentaria', 'Cancelado', '1', '2023-05-13 09:50:01', '2023-05-13 14:07:11');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `especialidade`
+-- Table structure for table `doctors`
 --
 
-CREATE TABLE `especialidade` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(220) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `doctors` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `speciality` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`id`, `name`, `gender`, `speciality`, `phone`, `email`, `image`, `created_at`, `updated_at`) VALUES
+(1, 'Cripton Baloi', 'M', '1', '846594587', 'criptonbaloi1@gmail.com', '1683622025.png', '2023-05-09 15:47:05', '2023-05-09 15:47:05'),
+(3, 'Gustavo', 'M', '1', '876594587', 'gustavo@gmail.com', '1683918247.png', '2023-05-13 02:04:07', '2023-05-13 02:04:07');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `medico`
+-- Table structure for table `failed_jobs`
 --
 
-CREATE TABLE `medico` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(220) NOT NULL,
-  `sexo` char(1) NOT NULL,
-  `id_especialidade` int(11) NOT NULL,
-  `email` varchar(220) NOT NULL,
-  `telefone` int(9) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `niveis_acesso`
+-- Table structure for table `migrations`
 --
 
-CREATE TABLE `niveis_acesso` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(220) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `niveis_acesso`
+-- Dumping data for table `migrations`
 --
 
-INSERT INTO `niveis_acesso` (`id`, `nome`, `created`, `modified`) VALUES
-(1, 'Administrador', '2023-04-05 00:00:00', NULL),
-(2, 'Medico', '2023-04-05 00:00:00', NULL),
-(3, 'Paciente', '2023-04-05 00:00:00', NULL);
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2014_10_12_200000_add_two_factor_columns_to_users_table', 1),
+(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+(5, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(6, '2023_05_07_052854_create_sessions_table', 1),
+(7, '2023_05_09_075852_create_doctors_table', 2),
+(8, '2023_05_13_021321_create_appointments_table', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `paciente`
+-- Table structure for table `password_reset_tokens`
 --
 
-CREATE TABLE `paciente` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(220) NOT NULL,
-  `apelido` varchar(220) NOT NULL,
-  `sexo` char(1) NOT NULL,
-  `telefone` int(9) NOT NULL,
-  `email` varchar(220) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `status_consulta`
+-- Table structure for table `personal_access_tokens`
 --
 
-CREATE TABLE `status_consulta` (
-  `id` int(11) NOT NULL,
-  `status` varchar(220) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Table structure for table `sessions`
 --
 
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `id_acesso` int(11) NOT NULL,
-  `email` varchar(220) NOT NULL,
-  `senha` varchar(220) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('AzfIp3MCTGg3all7cci0MuEVPEDU5wowjGhBe73T', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZkpoRUUzRWRrNDZqWWJDb0tnR3J6dkplR1pjUEp3b0RmaG10dEdiSiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9zaG93ZG9jdG9yIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6Mjt9', 1683965880);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `usertype` varchar(255) NOT NULL DEFAULT '0',
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `two_factor_secret` text DEFAULT NULL,
+  `two_factor_recovery_codes` text DEFAULT NULL,
+  `two_factor_confirmed_at` timestamp NULL DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `current_team_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `profile_photo_path` varchar(2048) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `address`, `usertype`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
+(1, 'Cripton Baloi', 'criptonbaloi9@gmail.com', '846594587', 'Bairro das Mahotas, Maputo, Mozambique', '0', NULL, '$2y$10$jj9KIlHJWNCqpu8T04EQ1.zBYEC8Jcp8Tfh6HMhXyKOisGvtvcRD6', NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-07 13:12:46', '2023-05-07 13:12:46'),
+(2, 'admin', 'admin@gmail.com', '846594587', 'Bairro das Mahotas, Maputo, Mozambique', '1', NULL, '$2y$10$3VdiFJRlzJi3V0ZdNA0ZYuiPqVcmoVPzqnmQsoGAHoCxL.B7vV9sy', NULL, NULL, NULL, NULL, NULL, NULL, '2023-05-07 13:15:58', '2023-05-07 13:15:58');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `agendamento`
+-- Indexes for table `appointments`
 --
-ALTER TABLE `agendamento`
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_paciente` (`id_paciente`),
-  ADD UNIQUE KEY `id_especialidade` (`id_especialidade`),
-  ADD UNIQUE KEY `id_medico` (`id_medico`),
-  ADD UNIQUE KEY `id_status` (`id_status`);
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
--- Indexes for table `especialidade`
+-- Indexes for table `migrations`
 --
-ALTER TABLE `especialidade`
+ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `medico`
+-- Indexes for table `password_reset_tokens`
 --
-ALTER TABLE `medico`
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_especialidade` (`id_especialidade`);
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
--- Indexes for table `niveis_acesso`
+-- Indexes for table `sessions`
 --
-ALTER TABLE `niveis_acesso`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `paciente`
---
-ALTER TABLE `paciente`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `status_consulta`
---
-ALTER TABLE `status_consulta`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `usuario`
---
-ALTER TABLE `usuario`
+ALTER TABLE `sessions`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_acesso` (`id_acesso`);
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `agendamento`
+-- AUTO_INCREMENT for table `appointments`
 --
-ALTER TABLE `agendamento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `appointments`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `especialidade`
+-- AUTO_INCREMENT for table `doctors`
 --
-ALTER TABLE `especialidade`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `doctors`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `medico`
+-- AUTO_INCREMENT for table `failed_jobs`
 --
-ALTER TABLE `medico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `niveis_acesso`
+-- AUTO_INCREMENT for table `migrations`
 --
-ALTER TABLE `niveis_acesso`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `paciente`
+-- AUTO_INCREMENT for table `personal_access_tokens`
 --
-ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `status_consulta`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `status_consulta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `agendamento`
---
-ALTER TABLE `agendamento`
-  ADD CONSTRAINT `fk_especialidade` FOREIGN KEY (`id_especialidade`) REFERENCES `especialidade` (`id`),
-  ADD CONSTRAINT `fk_medico` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id`),
-  ADD CONSTRAINT `fk_paciente` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id`),
-  ADD CONSTRAINT `fk_status` FOREIGN KEY (`id_status`) REFERENCES `status_consulta` (`id`);
-
---
--- Constraints for table `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `fk_acesso` FOREIGN KEY (`id_acesso`) REFERENCES `niveis_acesso` (`id`);
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
